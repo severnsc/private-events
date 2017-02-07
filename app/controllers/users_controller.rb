e74f72f0c6bcd2616697fe9d60900f999857effb
Chris Severns <severnsc@gmail.com>
2017-02-07 11:44:58 -0500

    Add users routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,24 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def index
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def delete
+	end
+
+	def destroy
+	end
+
 	private
 
 	def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :users, except: [:new, :create]
+
   get '/login', to: 'sessions#new'
 
   post '/login', to: 'sessions#create'
