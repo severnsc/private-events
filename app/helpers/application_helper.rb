3dace9a7007ed8706c01abd304702ee46183dae4
Chris Severns <severnsc@gmail.com>
2017-02-07 11:42:01 -0500

    Add User login, signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	def full_title(page_title = "")
+		base_title = "Private Events"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
+
+	def logged_in?
+		!session[:user_id].nil?
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+
+  post '/signup', to: 'users#create'
+
+  root "static_pages#home"
+
+  get "/about", to: 'static_pages#about'
 end
